5b31b33b1f3a317df4dd56f406a75bfae4a981c7
Erfanmah7 <efn8mah@yahoo.com>
2024-06-01 21:00:05 +0330

    add react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import ProductsPage from "./pages/ProductsPage";
+import PageNotFound from "./pages/PageNotFound";
+import ProductDetails from "./pages/ProductDetails";
+import Checkout from "./pages/Checkout";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to="/products" replace />} />
+          <Route path="/products" element={<ProductsPage />} />
+          <Route path="/products/:id" element={<ProductDetails />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
