4cc56469ea95950fc109d32cdb392410ad1157a7
Erfanmah7 <efn8mah@yahoo.com>
2024-06-28 14:08:06 +0330

    add case click shops

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -25,6 +25,52 @@ const reduce = (state, action) => {
         checkout: false,
       };
 
+    case "REMOVE":
+      const newSelectItems = state.selectItems.filter(
+        (item) => item.id !== action.payload.id
+      );
+
+      return {
+        ...state,
+        selectItems: [...newSelectItems],
+        ...sumProducts(newSelectItems),
+        checkout: false,
+      };
+
+    case "INCREASE":
+      const increaseIndex = state.selectItems.findIndex(
+        (item) => item.id === action.payload.id
+      );
+
+      state.selectItems[increaseIndex].quantity++;
+
+      return {
+        ...state,
+        ...sumProducts(state.selectItems),
+        checkout: false,
+      };
+
+    case "DECREASE":
+      const decreaseIndex = state.selectItems.findIndex(
+        (item) => item.id === action.payload.id
+      );
+
+      state.selectItems[decreaseIndex].quantity--;
+
+      return {
+        ...state,
+        ...sumProducts(state.selectItems),
+        checkout: false,
+      };
+
+    case "CHECKOUT":
+      return {
+        selectItems: [],
+        itemsCounter: 0,
+        total: 0,
+        checkout: true,
+      };
+
     default:
       throw new Error("invalid data");
   }
