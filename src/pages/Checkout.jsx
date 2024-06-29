9b8fdcdb6d47a2e3d69a96f78ebecbbb7fd5c0b9
Erfanmah7 <efn8mah@yahoo.com>
2024-06-30 00:35:05 +0330

    add checkout & style

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { useCart } from "../context/CartContext";
+import BasketCard from "../components/BasketCard";
 
 function Checkout() {
+  const [state, dispatch] = useCart();
+
+  const clickHandler = (type, payload) => dispatch({ type, payload });
+
   return (
-    <div>Checkout</div>
-  )
+    <div>
+      {state.selectItems.map((product) => (
+        <BasketCard
+          key={product.id}
+          data={product}
+          clickHandler={clickHandler}
+        />
+      ))}
+    </div>
+  );
 }
 
-export default Checkout
+export default Checkout;
